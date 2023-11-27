c4d1fc44e18bc9895f3c6bf4362b15605ef94b76
Jean Paul Mamani Motta <jmamanim@ulasalle.edu.pe>
2023-11-27 14:30:55 -0500

    Aplicacion Movil

--- App.js
+++ App.js
@@ -1,11 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Button, StyleSheet, Text } from 'react-native';
+import * as DocumentPicker from 'expo-document-picker';
+import { Audio } from 'expo-av';
+
 
 export default function App() {
+  const [sound, setSound] = useState();
+  const [isPlaying, setIsPlaying] = useState(false);
+
+
+  const playSound = async () => {
+    try {
+      if (sound) {
+        if (isPlaying) {
+          await sound.pauseAsync();
+          setIsPlaying(false);
+          console.log('Audio paused');
+        } else {
+          await sound.playAsync();
+          setIsPlaying(true);
+          console.log('Audio playing');
+        }
+      }
+    } catch (error) {
+      console.log('Error playing audio:', error);
+    }
+  };
+
+  const pickAudio = async () => {
+    try {
+      const result = await DocumentPicker.getDocumentAsync({ type: 'audio/*', copyToCacheDirectory: false });
+      if (result.canceled == 'true') return;
+
+
+      const { uri } = result["assets"][0];
+      const { sound: audioSound } = await Audio.Sound.createAsync({ uri });
+      setSound(audioSound);
+      console.log('Audio selected:', uri);
+    } catch (error) {
+      console.log('Error selecting audio:', error);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      
+      <Text>
+        Select an Audio
+      </Text>
+      <Button title="Select Audio" onPress={pickAudio} />
+      <Text>
+        Play this audio
+      </Text>
+      <Button title={isPlaying ? 'Pause' : 'Play'} onPress={playSound} />
     </View>
   );
 }
@@ -13,8 +60,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
   },
 });
